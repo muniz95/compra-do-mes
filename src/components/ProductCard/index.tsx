2d729fe27f62fd3ff5f04677eb47feb0f947dfb3
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2020-08-28 00:55:41 -0300

    Remoção de componentes class-based e ajustes menores

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,106 +1,70 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import act from "../../redux/actions";
 import Product from "../../models/Product";
 import S from "./styled";
 
-interface IProps {
-  product: Product;
-  dispatchDecreaseTotal: (total: number) => {};
-  dispatchEditProduct: (product: Product) => {};
-  dispatchRemoveProduct: (product: Product) => {};
-}
+const ProductCard = (product: Product) => {
+  const dispatch = useDispatch();
+  const dispatchDecreaseTotal = React.useCallback(
+    (total) => dispatch(act.decreaseTotal(total)),
+    [dispatch]
+  );
+  const dispatchEditProduct = React.useCallback(
+    (total) => dispatch(act.editProduct(total)),
+    [dispatch]
+  );
+  const dispatchRemoveProduct = React.useCallback(
+    (product) => dispatch(act.removeProduct(product)),
+    [dispatch]
+  );
 
-class ProductComponent extends React.Component<IProps, {}> {
-  constructor(props: IProps) {
-    super(props);
-
-    this.removeProduct = this.removeProduct.bind(this);
-    this.editProduct = this.editProduct.bind(this);
-    this.capitalize = this.capitalize.bind(this);
-  }
-
-  public editProduct(): void {
-    const { product } = this.props;
-    this.props.dispatchDecreaseTotal(product.total);
-    this.props.dispatchEditProduct(product);
+  const editProduct = () => {
+    dispatchDecreaseTotal(product.total);
+    dispatchEditProduct(product);
     window.scrollTo(0, 0);
   }
 
-  public removeProduct(): void {
-    const { product } = this.props;
-    this.props.dispatchDecreaseTotal(product.total);
-    this.props.dispatchRemoveProduct(product);
+  const removeProduct = () => {
+    dispatchDecreaseTotal(product.total);
+    dispatchRemoveProduct(product);
   }
 
-  public capitalize(word: string) {
+  const capitalize = (word: string) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
 
-  public render(): JSX.Element {
-    return (
-      <S.ProductCardContainer>
-        <S.ProductCardBody>
-          <S.ProductCardColumn style={{width: '30%'}}>
-            <h2>{this.capitalize(this.props.product.name)}</h2>
-          </S.ProductCardColumn>
-          <S.ProductCardColumn style={{width: '40%'}}>
-            <S.ProductCardRow>
-              <S.ProductCardColumn>Qtd.</S.ProductCardColumn>
-              <S.ProductCardColumn>Preço</S.ProductCardColumn>
-              <S.ProductCardColumn>Total</S.ProductCardColumn>
-            </S.ProductCardRow>
-            <S.ProductCardRow>
-              <S.ProductCardColumn>{this.props.product.quantity}</S.ProductCardColumn>
-              <S.ProductCardColumn>R$ {this.props.product.price}</S.ProductCardColumn>
-              <S.ProductCardColumn>R$ {this.props.product.total}</S.ProductCardColumn>
-            </S.ProductCardRow>
-          </S.ProductCardColumn>
-          <S.ProductCardColumn style={{width: '30%', height: '100%'}}>
-            <S.ProductCardRow>
-              <S.ProductCardColumn>
-                <i className="fa fa-2x fa-pencil" onClick={this.editProduct}></i>
-              </S.ProductCardColumn>
-              <S.ProductCardColumn>
-                <i className="fa fa-2x fa-trash" onClick={this.removeProduct}></i>
-              </S.ProductCardColumn>
-            </S.ProductCardRow>
-          </S.ProductCardColumn>
-        </S.ProductCardBody>
-      </S.ProductCardContainer>
-      // <div>
-      //   <div className="card-block">
-      //     <h4 className="card-title">{this.props.product.name}</h4>
-      //     <div className="row">
-      //       <div className="col"><b>Qtd.</b></div>
-      //       <div className="col"><b>Preço</b></div>
-      //       <div className="col"><b>Preço total</b></div>
-      //     </div>
-      //     <div className="row">
-      //       <div className="col">{this.props.product.quantity}</div>
-      //       <div className="col">R$ {this.props.product.price}</div>
-      //       <div className="col">R$ {this.props.product.total}</div>
-      //     </div>
-      //     <div className="row">&nbsp;</div>
-      //     <div className="row">
-      //       <div className="col text-center">
-      //         <i className="fa fa-2x fa-pencil" onClick={this.editProduct}></i>
-      //       </div>
-      //       <div className="col text-center">
-      //         <i className="fa fa-2x fa-trash" onClick={this.removeProduct}></i>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
-    );
-  }
+  return (
+    <S.ProductCardContainer>
+      <S.ProductCardBody>
+        <S.ProductCardColumn style={{width: '30%'}}>
+          <h2>{capitalize(product.name)}</h2>
+        </S.ProductCardColumn>
+        <S.ProductCardColumn style={{width: '40%'}}>
+          <S.ProductCardRow>
+            <S.ProductCardColumn>Qtd.</S.ProductCardColumn>
+            <S.ProductCardColumn>Preço</S.ProductCardColumn>
+            <S.ProductCardColumn>Total</S.ProductCardColumn>
+          </S.ProductCardRow>
+          <S.ProductCardRow>
+            <S.ProductCardColumn>{product.quantity}</S.ProductCardColumn>
+            <S.ProductCardColumn>R$ {product.price}</S.ProductCardColumn>
+            <S.ProductCardColumn>R$ {product.total}</S.ProductCardColumn>
+          </S.ProductCardRow>
+        </S.ProductCardColumn>
+        <S.ProductCardColumn style={{width: '30%', height: '100%'}}>
+          <S.ProductCardRow>
+            <S.ProductCardColumn>
+              <i className="fa fa-2x fa-pencil" onClick={editProduct}></i>
+            </S.ProductCardColumn>
+            <S.ProductCardColumn>
+              <i className="fa fa-2x fa-trash" onClick={removeProduct}></i>
+            </S.ProductCardColumn>
+          </S.ProductCardRow>
+        </S.ProductCardColumn>
+      </S.ProductCardBody>
+    </S.ProductCardContainer>
+  );
 }
 
-const mapDispatchToProps = {
-  dispatchDecreaseTotal: act.decreaseTotal,
-  dispatchEditProduct: act.editProduct,
-  dispatchIncreaseTotal: act.increaseTotal,
-  dispatchRemoveProduct: act.removeProduct,
-};
-
-export default connect(null, mapDispatchToProps)(ProductComponent);
+export default ProductCard;
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.scss";
-import Main from "./Main";
 import { store } from "./redux/store";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Main>
-      <App />
-    </Main>
+    <App />
   </Provider>
   , document.getElementById("app")
 );
